5522b8a460598c2cdf367bc30a46c64bf1dadbbd
Joel <joedeathsycthe@gmail.com>
2017-10-30 13:13:54 -0300

    segurança implementada, comentario de security initializer

--- spring-mvc-pizzaria/src/com/joelnetodev/pizzaria/StartSecurityApplication.java
+++ spring-mvc-pizzaria/src/com/joelnetodev/pizzaria/StartSecurityApplication.java
@@ -2,5 +2,6 @@ package com.joelnetodev.pizzaria;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+//Para que a segurança funcione, precisa ter uma classe que herde de SecurityWebInitializer, assim como a WebAppInitializer
 public class StartSecurityApplication extends AbstractSecurityWebApplicationInitializer {
 }
